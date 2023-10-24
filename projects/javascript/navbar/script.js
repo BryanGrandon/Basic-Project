5893ac2ef68cd3ea6ee16a9580fef32e8e7a80bb
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-10-24 16:18:37 -0300

    navbar update

--- projects/javascript/navbar/script.js
+++ projects/javascript/navbar/script.js
@@ -1,11 +1,11 @@
 const d = document;
 
-const toggleBtn = d.querySelector(".toggle_btn");
-const toggleBtnIcon = d.querySelector(".toggle_btn i");
+const toggleBtn = d.querySelector(".navbar__toggle_btn");
+const toggleBtnIcon = d.querySelector(".navbar__toggle_btn i");
 const dropdownMenu = d.querySelector(".dropdown_menu");
 
 toggleBtn.addEventListener("click", () => {
-  dropdownMenu.classList.toggle("open");
-  const isOpen = dropdownMenu.classList.contains("open");
+  dropdownMenu.classList.toggle("dropdown_menu--open");
+  const isOpen = dropdownMenu.classList.contains("dropdown_menu--open");
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 });
