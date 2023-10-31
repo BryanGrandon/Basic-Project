a8a18445bbe76b63971f87f134c42ad039708460
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-10-31 18:27:25 -0300

    json-placeholder updated

--- projects/api/json-placeholder/users.js
+++ projects/api/json-placeholder/users.js
@@ -1,35 +1,28 @@
 const url = "https://jsonplaceholder.typicode.com/";
 const root = document.getElementById("root");
 
-const createUserTable = (json) => {
-  const $fragmentUsers = document.createDocumentFragment();
+const createTable = (json) => {
+  const $fragment = document.createDocumentFragment();
+  const $template = document.querySelector(".template__table").content;
+  const $clone = $template.cloneNode(true);
 
-  const $thead = document.createElement("thead");
-  $thead.classList = "table__head";
-  $thead.innerHTML = `
+  $clone.querySelector(".table__thead").innerHTML = `
   <tr>
     <th>Name</th>
     <th>Email</th>
   </tr>`;
-  $fragmentUsers.appendChild($thead);
 
-  const $tbody = document.createElement("tbody");
-  $tbody.classList = "table__body";
   json.forEach((e) => {
-    const $tr = document.createElement("tr");
-    $tr.classList = "table__user";
-    $tr.innerHTML = `
-    <td class="table__user__name">${e.name}</td>
-    <td class="table__user__email">${e.email}</td>
-  `;
-    $tbody.appendChild($tr);
+    const text = `
+    <tr>
+      <td class="table__user__name">${e.name}</td>
+      <td class="table__user__email">${e.email}</td>
+    </tr>
+    `;
+    $clone.querySelector(".table__tbody").innerHTML += text;
   });
-  $fragmentUsers.appendChild($tbody);
-
-  const $table = document.createElement("table");
-  $table.classList = "table";
-  $table.appendChild($fragmentUsers);
-  root.appendChild($table);
+  $fragment.appendChild($clone);
+  root.appendChild($fragment);
 };
 
 const loadUsers = async (url) => {
@@ -37,7 +30,7 @@ const loadUsers = async (url) => {
     const response = await fetch(url + "users");
     if (response.status === 200) {
       const json = await response.json();
-      createUserTable(json);
+      createTable(json);
     } else if (response.status === 404) console.log("Error 404");
   } catch (error) {
     console.info(error);
