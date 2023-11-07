d84711f8391f83af651f984b952cbdc39baec9f0
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-11-07 15:39:55 -0300

    network-status added in website

--- documents/javascript-docs/website.md
+++ documents/javascript-docs/website.md
@@ -1,3 +1,4 @@
 # Website
 
 - [Navbar](/projects/javascript/website/navbar/navbar.html)
+- [Network Status](/projects/javascript/website/network-status/network-status.html)
--- projects/javascript/website/network-status/network-status-script.js
+++ projects/javascript/website/network-status/network-status-script.js
@@ -1,14 +1,15 @@
-const d = document,
-  w = window,
-  n = navigator;
+"use strict";
+const d = document;
+const w = window;
+const n = navigator;
 
-function networkStatus() {
+const networkStatus = () => {
   const isOnline = () => {
     const $section = d.createElement("section");
     if (n.onLine) {
       $section.textContent = `Connection restored`;
-      $section.classList.add("online");
       $section.classList.remove("offline");
+      $section.classList.add("online");
     } else {
       $section.textContent = `Lost connection`;
       $section.classList.remove("online");
@@ -17,10 +18,11 @@ function networkStatus() {
     d.body.insertAdjacentElement("afterbegin", $section);
     setTimeout(() => {
       d.body.removeChild($section);
-    }, 2000);
+    }, 1000);
   };
   w.addEventListener("online", (e) => isOnline());
   w.addEventListener("offline", (e) => isOnline());
-}
-
+};
 networkStatus();
+
+console.log("ddd");
