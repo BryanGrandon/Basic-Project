f412ff4c6d59adc271345fa371aac0592eef35bf
Bryan Grandon <bryan.grandon.c@gmail.com>
2023-11-29 13:18:24 -0300

    refactor(web): add 02-click-counter in react  and remove projects

--- projects/react/projects/03-click-counter/src/components/Counter.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/Counter.css";
-
-function Counter({ clicksNumber }) {
-  return <article className="counter">{clicksNumber}</article>;
-}
-
-export default Counter;
