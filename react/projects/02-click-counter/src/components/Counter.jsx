4d3d6f860b5a2f250b2d63e8f7b8a41d7ce74541
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-04-24 18:56:53 -0400

    (web):remove files

--- react/projects/02-click-counter/src/components/Counter.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "../styles/Counter.css";
-function Counter({ numberClick }) {
-  return <article className="counter">{numberClick}</article>;
-}
-export default Counter;
--- react/projects/03-calculator/src/components/Screen.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/Screen.css";
-
-const Screen = ({ input }) => {
-  return <section className="screen__input">{input}</section>;
-};
-
-export default Screen;
