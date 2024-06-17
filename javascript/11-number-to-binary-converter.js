073ca991e3044edc0be4fb8058754d7cb75332cc
Bryan Grandon <bryan.grandon.c@gmail.com>
2024-06-17 16:39:31 -0400

    feat(web):change problem 11 to update the structure and add new functions

--- javascript/11-number-to-binary-converter.js
+++ javascript/11-number-to-binary-converter.js
@@ -1,25 +1,53 @@
-const binary = (number = 0) => {
-  let numberToBinary =
-    typeof number == "string" || typeof number == "number"
-      ? Number(number)
-      : undefined;
+const wholeNumber = (number = 0) => {
+  const binary = [];
+  if (number == 0) binary.push(number);
+  while (number > 0) {
+    binary.push(number % 2);
+    number = Math.floor(number / 2);
+  }
+  return binary.reverse().join("");
+};
 
-  if (numberToBinary === undefined) {
-    console.error("The value entered is not a number");
+const decimalNumber = (number) => {
+  const decimal = String(number).split(".");
+  let binary = [];
+  let result = [];
+  for (let i = 0; decimal.length > i; i++) {
+    while (decimal[i] > 0) {
+      result.push(decimal[i] % 2);
+      decimal[i] = Math.floor(decimal[i] / 2);
+    }
+    binary.push(result.reverse().join(""));
+    result = [];
+  }
+  return binary.join(".");
+};
+
+const binary = (number = 0) => {
+  let numberToBinary;
+  if (typeof number == "number") numberToBinary = number;
+  else if (typeof number == "string") {
+    let string = Number(number);
+    numberToBinary = string == number ? string : number;
+  } else numberToBinary = null;
+
+  if (typeof numberToBinary != "number") {
+    console.error(`The value entered "${numberToBinary}" is not a number`);
     return null;
   }
 
-  const array = [];
-  if (numberToBinary == 0) array.push(numberToBinary);
+  const isDecimal = String(numberToBinary).split(".").length > 1;
 
-  while (numberToBinary > 0) {
-    array.push(numberToBinary % 2);
-    numberToBinary = Math.floor(numberToBinary / 2);
-  }
-
-  return array.reverse().join("");
+  return isDecimal
+    ? decimalNumber(numberToBinary)
+    : wholeNumber(numberToBinary);
 };
 
-console.log(binary(8));
-console.log(binary(80));
-console.log(binary(9023));
+const example = {
+  5: binary(5),
+  80: binary("80"),
+  21.5: binary(21.5),
+  string: binary("string"),
+};
+
+console.table(example);
